a96e04fa0a874b652d7fe07d9369ae813b746492
jamalss <jamal8010@gmail.com>
2020-03-09 19:14:23 -0400

    dl bootstrap and make varied changes to .html and .ts. Like i said practicing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Paragraph3 } from './practicers';
+
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My First Practice Page';
   subtitle = 'Practicing makes lives happy';
-  paragraph = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
+  paragraph = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   
   subtitle2 = 'Practicing they essence of it all';
-  paragraph2 = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
+  paragraph2 = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon.There are these that they stray say. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   
   subtitle3 = 'Practicing and me';
-  paragraph3 = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
+  paragraph3: Paragraph3 = {
+	id: 1,
+	name: 'Practicer',
+	details: 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon.Then there was one whom toumb doom room.There are these that they stray say.'
+	};
 }
