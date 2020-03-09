7840b22a4db3fc16486f9e41cd6081ec3525504c
jamalss <jamal8010@gmail.com>
2020-03-09 17:40:44 -0400

    add more tags/elements to main html page, then some css to the webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'My First Practice Page';
   subtitle = 'Practicing makes lives happy';
+  paragraph = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
+  
+  subtitle2 = 'Practicing they essence of it all';
+  paragraph2 = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
+  
+  subtitle3 = 'Practicing and me';
+  paragraph3 = 'Lorem ipsum ipsum lorem. Therum bereaum fom home some come soon. Then there was one whom toumb doom room. There are these that they stray say. Thus';
 }
